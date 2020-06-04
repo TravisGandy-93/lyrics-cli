025ed74d8ab85e7b6bd8f2b60a91e13748568c75
Travis Gandy <travis_gandy@yahoo.com>
2020-06-04 19:17:40 +0000

    app environment update

--- lib/lyrics_cli.rb
+++ lib/lyrics_cli.rb
@@ -1,4 +1,8 @@
 #application environment
+
+require 'bundler/setup'
+Bundler.require(:default)
+
 require_relative './lyrics_cli/cli.rb'
 require_relative './lyrics_cli/api.rb'
 require_relative './lyrics_cli/lyrics.rb'
