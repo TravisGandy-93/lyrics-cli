032cbf41928574a2407dc92d0c4417218e20c762
Travis Gandy <travis_gandy@yahoo.com>
2020-06-04 19:08:48 +0000

    deleted unnecessary file

--- lib/lyrics/cli.rb
+++
@@ -1,8 +0,0 @@
-require "lyrics/cli/version"
-
-module Lyrics
-  module Cli
-    class Error < StandardError; end
-    # Your code goes here...
-  end
-end
--- lib/lyrics/cli/version.rb
+++
@@ -1,5 +0,0 @@
-module Lyrics
-  module Cli
-    VERSION = "0.1.0"
-  end
-end
